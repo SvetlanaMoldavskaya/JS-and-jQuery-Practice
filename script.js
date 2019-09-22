c0760c36c08bc060af1f03741c13b4a54a0b0e4f
SvetlanaMd <SvetlanaMoldavskaya@users.noreply.github.com>
2019-09-22 15:56:23 +0300

    Added Arrays practice examples
    Arrays:
    -push
    -pop
    -shift
    -unshift
    -index

--- script.js
+++ script.js
@@ -1,41 +1,57 @@
-$(document).ready(function(){
-console.log("he");
+$(document).ready(function() {
+  console.log("he");
 
-function adder(num1, num2) {
-  console.log(num1 + num2);
-}
-adder(5, 10);
+  function adder(num1, num2) {
+    console.log(num1 + num2);
+  }
+  adder(5, 10);
 
-var name = "Benjamin";
-var town = "San-Francisco";
+  var name = "Benjamin";
+  var town = "San-Francisco";
 
-function makeBig(text) {
-  console.log(text.toUpperCase());
-}
-makeBig(name);
-makeBig(town);
+  function makeBig(text) {
+    console.log(text.toUpperCase());
+  }
+  makeBig(name);
+  makeBig(town);
 
-const makeLettersBig = function(text) {
-  console.log(text.toUpperCase());
-};
-makeLettersBig("funny");
+  const makeLettersBig = function(text) {
+    console.log(text.toUpperCase());
+  };
+  makeLettersBig("funny");
 
-const toUpper = text => {
-  console.log(text.toUpperCase());
-};
+  const toUpper = text => {
+    console.log(text.toUpperCase());
+  };
 
-toUpper("how dare you?");
-/////////////
-$("#button1").on("click", function() {
-  $("#p1").css("opacity", 0);
-});
+  toUpper("how dare you?");
+  /////////////
+  $("#button1").on("click", function() {
+    $("#p1").css("opacity", 0);
+  });
 
-///////////////////
+  ///////////////////
 
-  $("#hide").click(function(){
+  $("#hide").click(function() {
     $("p").hide();
   });
-  $("#show").click(function(){
+  $("#show").click(function() {
     $("p").show();
   });
 });
+/////////////////////////
+
+const schedule = ["wake up", "eat", "watch smth", "clean"];
+
+console.log(schedule);
+console.log(schedule[0]);
+schedule.push("boom");
+console.log(schedule);
+
+schedule.pop();
+console.log(schedule);
+console.log(schedule.indexOf("eat"));
+
+const watch = schedule.indexOf("watch smth");
+console.log(watch);
+console.log(schedule[watch]);
